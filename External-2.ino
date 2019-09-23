6c69e4b2376dd3f0acf19f61b7affdb959c3be96
Sharonwg26 <49950909+Sharonwg26@users.noreply.github.com>
2019-09-23 10:42:07 +0800

    Add files via upload

---
+++ External-2.ino
@@ -0,0 +1,19 @@
+boolean led;
+void setup() {
+  led = true;
+  pinMode(2,OUTPUT);
+  pinMode(8,OUTPUT);
+}
+
+void loop() {
+  if(led){
+    led = false;
+    digitalWrite(2,HIGH);
+    digitalWrite(8,LOW);
+  }else{
+    led = true;
+    digitalWrite(2,LOW);
+    digitalWrite(8,HIGH);
+  }
+  delay(1000);
+}
