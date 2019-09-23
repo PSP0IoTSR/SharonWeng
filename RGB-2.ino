d130ec72ba4c02a7795503e8e158f7850d9680b2
Sharonwg26 <49950909+Sharonwg26@users.noreply.github.com>
2019-09-23 10:57:39 +0800

    Add files via upload

---
+++ RGB-2.ino
@@ -0,0 +1,12 @@
+void setup(){
+  pinMode(15, OUTPUT);
+  pinMode(16, OUTPUT);
+  pinMode(17, OUTPUT);
+}
+
+void loop(){
+  analogWrite(15, (random(255)));
+  analogWrite(16, (random(255)));
+  analogWrite(17, (random(255)));
+  delay(1000);
+}
